1eb08056ad9461a27c935d714bb19067a90cfe2e
Jose Casanova <jacapersonal04@gmail.com>
2021-06-10 17:11:57 -0400

    added form and model forms to the app

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     """Post model."""
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.profile', on_delete=models.CASCADE)
 
     title = models.CharField(max_length=250)
     photo = models.ImageField(upload_to='posts/photos')
