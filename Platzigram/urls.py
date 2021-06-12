3d1c46e9fad4a88c347ab2372c569e701a36fbb4
Jose Casanova <jacapersonal04@gmail.com>
2021-06-12 13:29:31 -0400

    Class based views

--- Platzigram/urls.py
+++ Platzigram/urls.py
@@ -1,13 +1,12 @@
 """Platigram URLs module."""
 #Django
+from django import urls
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 
-from Platzigram import views as local_views
-from posts import views as posts_views
 from users import views as users_views
 from users import views as logout_view
 
@@ -15,17 +14,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
 
-    path('world/', local_views.hiword, name='world'),
-    path('sorted/', local_views.sort_integers, name='sort'),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
-
-    path('', posts_views.list_posts, name='feed'),
-    path('posts/new/', posts_views.create_posts, name='create_post'),
-
-    path('users/login/', users_views.login_view, name='login'),
-    path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup_view, name='signup'),
-    path('users/me/profile/', users_views.update_profile, name='update_profile'),
-
+    path('', include(('posts.urls', 'posts'), namespace='posts')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
